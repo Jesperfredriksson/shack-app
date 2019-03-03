0eda088baf321472330efbb9abe67943d0771661
Jesper Fredriksson <jesperfredriksson@live.se>
2019-03-03 16:45:45 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
-import Foodplaces from "./Foodplaces";
+
+const RESTURANTS = [
+  "Phil's",
+  "Bastards",
+  "Pastastället",
+  "Borgen",
+  "Icasallad"
+];
 
 class App extends Component {
+  state = {
+    currentResturant: undefined
+  };
+
+  getRandomResturant = () => {
+    const randomIndex = Math.floor(Math.random() * RESTURANTS.length);
+    return RESTURANTS[randomIndex];
+  };
+
+  componentDidMount = () => {
+    this.setState({ currentResturant: this.getRandomResturant() });
+  };
+
+  handleClick = () => {
+    this.setState({ currentResturant: this.getRandomResturant() });
+  };
+
   render() {
     return (
       <div className="site-container">
         <div className="title">
           <h2>Var ska vi äta idag?</h2>
         </div>
-        <div className="content">
-          <Foodplaces />
-        </div>
+        <div className="content">{this.state.currentResturant}</div>
+        <button onClick={this.handleClick}>
+          Fel ställe? Tryck här tills alla är nöjda
+        </button>
       </div>
     );
   }
